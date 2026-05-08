98862d9e4105e7167c6a504e4be6a06ed5fe530a
syukri zainal <syukrizainal14@gmail.com>
2026-05-08 22:32:17 +0800

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,39 +2,31 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    # URL API Watson NLP
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    
-    # Headers untuk model AI
+    url = 'https://sn-watson-emotion.p.cloud.ibm.com/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    
-    # Payload (Teks yang hendak dianalisis)
     myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers=header)
     
-    # Menghantar permintaan ke API
-    response = requests.post(url, json=myobj, headers=header)
-    
-    # Menukar respons teks kepada JSON (Dictionary)
-    formatted_response = json.loads(response.text)
-
-    # Ekstrak data emosi (Logik untuk Task 3)
-    if response.status_code == 200:
-        emotions = formatted_response['emotionPredictions'][0]['emotion']
-        
-        # Cari emosi dominan (skor paling tinggi)
-        dominant_emotion = max(emotions, key=emotions.get)
-
-        # Susun output akhir
-        result = {
-            'anger': emotions['anger'],
-            'disgust': emotions['disgust'],
-            'fear': emotions['fear'],
-            'joy': emotions['joy'],
-            'sadness': emotions['sadness'],
-            'dominant_emotion': dominant_emotion
+    # Kena ada ni supaya tak keluar error 500 tadi
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
         }
-    else:
-        # Jika API bermasalah (Task 7 persiapan)
-        result = None
 
-    return result
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
