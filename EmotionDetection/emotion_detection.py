ed1409e64b58a66259c44f7c2bb8f0a5ba16e434
syukri zainal <syukrizainal14@gmail.com>
2026-05-08 22:51:53 +0800

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,15 +7,11 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
     
-    # INI BAHAGIAN PALING PENTING!
+    # Jika input kosong atau error
     if response.status_code == 400:
         return {
-            'anger': None,
-            'disgust': None,
-            'fear': None,
-            'joy': None,
-            'sadness': None,
-            'dominant_emotion': None
+            'anger': None, 'disgust': None, 'fear': None, 
+            'joy': None, 'sadness': None, 'dominant_emotion': None
         }
 
     formatted_response = json.loads(response.text)
